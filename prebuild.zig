d86bd69c4a8116d7aa46985e4039f86244522424
frmdstryr <frmdstryr@protonmail.com>
2020-10-31 13:32:54 -0400

    Update prebuild

--- prebuild.zig
+++ prebuild.zig
@@ -1,18 +1,22 @@
 const std = @import("std");
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 pub fn main() !void {
     const dest_folder = "zig-packages";
+    defer std.debug.assert(!gpa.deinit());
 
-    try run(&[_][]const u8{
-        "wget", "https://github.com/frmdstryr/zhp/archive/master.zip", "-O",
-        "zhp.zip"});
-    try run(&[_][]const u8{"unzip", "zhp.zip", "-d", dest_folder});
-    try run(&[_][]const u8{"rm", "zhp.zip"});
+    try run(.{"wget", "https://github.com/frmdstryr/zhp/archive/master.zip", "-O", "zhp.zip"});
+    try run(.{"unzip", "zhp.zip", "-d", dest_folder});
+    try run(.{"rm", "zhp.zip"});
 }
 
 
-pub fn run(cmd: [][]const u8) !void {
-    var process = try std.ChildProcess.init(cmd, std.heap.page_allocator);
+pub fn run(args: anytype) !void {
+    var cmd: [args.len][]const u8 = undefined;
+    inline for(args) |arg, i| {
+        cmd[i] = arg;
+    }
+    var process = try std.ChildProcess.init(cmd[0..], std.heap.page_allocator);
     try process.spawn();
     _ = try process.wait();
 }
